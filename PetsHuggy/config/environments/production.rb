e2e42d006e11391f9d486aba066a7ec6e90b5484
Rina Mito <satoshikibu@gmail.com>
2018-04-27 00:12:49 +0900

    part 2-1

--- PetsHuggy/config/environments/production.rb
+++ PetsHuggy/config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+# remenber to change localhost to actual host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
