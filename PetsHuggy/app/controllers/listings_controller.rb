1b2d4dd93d27cfc0ce954dc36a8f737b69c65769
Rina Mito <satoshikibu@gmail.com>
2018-05-08 12:44:49 +0900

    part 4-5-2

--- PetsHuggy/app/controllers/listings_controller.rb
+++ PetsHuggy/app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
 before_action :authenticate_user!
+before_action :set_listing, only: [:basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
+
 
   def index
   end
@@ -29,16 +31,34 @@ before_action :authenticate_user!
   end
 
   def basics
-    @listing = Listing.find(params[:id])
   end
 
   def description
-    @listing = Listing.find(params[:id])
+  end
+
+  def address
+  end
+
+  def price
+  end
+
+  def photos
+  end
+
+  def calendar
+  end
+
+  def bankaccount
+  end
+
+  def publish
   end
 
   private
   def listing_params
     params.require(:listing).permit(:home_type, :pet_type, :breeding_years,  :pet_size)
   end
-
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
 end
