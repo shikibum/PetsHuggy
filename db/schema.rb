dba3d7ef5579521e8b76dc118402c2ec171ee992
Rina Mito <satoshikibu@gmail.com>
2018-06-08 17:56:56 +0900

    part 16-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_055939) do
+ActiveRecord::Schema.define(version: 2018_06_08_085338) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "home_type"
@@ -29,6 +36,16 @@ ActiveRecord::Schema.define(version: 2018_06_06_055939) do
     t.float "longitude"
     t.integer "not_checked"
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
