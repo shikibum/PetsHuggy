3ae2ca10b11742093e58f896e4f0ef06ac801297
Rina Mito <satoshikibu@gmail.com>
2018-06-15 15:16:02 +0900

    part 18-3-4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,18 @@ class PagesController < ApplicationController
   end
 
   def search
+    if params[:search].present?
+
+      @latitude = params["lat"]
+      @longitude = params["lng"]
+
+      # 検索欄が空の場合
+    else
+
+      @listings = Listing.where(active: true).all
+      @latitude = @listings.to_a[0].latitude
+      @longitude = @listings.to_a[0].longitude
+
+    end
   end
 end
