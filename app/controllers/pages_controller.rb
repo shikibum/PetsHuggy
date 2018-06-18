f6c985fea9a662a7c78dc6bf0309864d51ebf660
Rina Mito <satoshikibu@gmail.com>
2018-06-18 21:50:23 +0900

    part 18-4-5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,5 +27,34 @@ class PagesController < ApplicationController
       @longitude = @listings.to_a[0].longitude
 
     end
+
+    #リスティングデータを配列にしてまとめる
+    @arrlistings = @listings.to_a
+
+    # start_date end_dateの間に予約がないことを確認.あれば削除
+    if ( !params[:start_date].blank? && !params[:end_date].blank? )
+
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      @listings.each do |listing|
+
+        # check the listing is availble between start_date to end_date
+        unavailable = listing.reservations.where(
+            "(? <= start_date AND start_date <= ?)
+              OR (? <= end_date AND end_date <= ?)
+              OR (start_date < ? AND ? < end_date)",
+            start_date, end_date,
+            start_date, end_date,
+            start_date, end_date
+        ).limit(1)
+
+        # delete unavailable room from @listings
+        if unavailable.length > 0
+          @arrlistings.delete(listing)
+        end
+      end
+    end
+    
   end
 end
